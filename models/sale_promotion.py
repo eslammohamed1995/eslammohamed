d05b73585f4283c611de487d2f5b3ee0cd02275a
eslammohamedhelmy-git <70444790+eslammohamedhelmy-git@users.noreply.github.com>
2020-09-02 17:12:54 +0200

    Update sale_promotion.py

--- models/sale_promotion.py
+++ models/sale_promotion.py
@@ -6,30 +6,27 @@ class promotion(models.Model):
     _name = 'sale.promotion'
     _description = 'sale_promotion'
 
-    product = fields.Many2one("product.product", string="name of product",
-                              ondelete='set null')
+    product = fields.Selection(selection=[
+        ('draft', 'Tea'),
+        ('orange', 'Orange juice'),
+    ], string='product', required=True)
 
-    quantity_2 = fields.Float(string="quantity", default=1.00)
+    quantity = fields.Float(string="quantity", default=1.00)
     offer = fields.Float(string="offer", readonly=True, store=True)
-    total_quantity = fields.Float(string="total quantity", compute="_total_quantity", store=True)
-    price_2 = fields.Float(string="Price", readonly=True, store=True)
-    total_2 = fields.Float(string="total", store=True)
+    price = fields.Float(string="Price", store=True)
+    total = fields.Float(string="total", store=True)
+
+    @api.onchange('price', 'quantity')
+    def _total_price(self):
+        for r in self:
+            r.total = r.price * r.quantity
 
     @api.onchange('product')
     def _price_change(self):
-        self.price_2 = self.product.list_price
+        if self.product == "draft":
+            self.price = 10.00
+        elif self.product == "orange":
+            self.price = 20.00
 
-    @api.depends('quantity_2')
-    def _total_quantity(self):
-        for r in self:
-            if r.quantity_2 > 2:
-                r.total_quantity = r.quantity_2 + 1
-                r.offer = 1.00
-            else:
-                r.total_quantity = r.quantity_2
-                r.offer = 0.00
 
-    @api.onchange('price_2', 'quantity_2')
-    def _total_price_2(self):
-        for r in self:
-            r.total_2 = r.quantity_2 * r.price_2
+
