e2dcc8bf26fd3801eb825c2170454b588a02b817
eslammohamedhelmy-git <70444790+eslammohamedhelmy-git@users.noreply.github.com>
2020-09-06 10:22:29 +0200

    Update models.py

--- models/models.py
+++ models/models.py
@@ -6,6 +6,7 @@ from odoo import models, fields, api
 class order(models.Model):
     _name = 'coffee.order'
     _description = 'coffee_order'
+    _rec_name = "customer"
 
     customer = fields.Many2one("res.partner", ondelet="set null", string="customer name",
                                required=True)
@@ -20,6 +21,18 @@ class order(models.Model):
         result = super(order, self).create(vals)
         return result
 
+    def confirm(self):
+        for r in self.env['sale.promotion'].search([]):
+            for line in self.line_ids:
+                if (line.product_id == r.product) & (line.quantity == r.quantity):
+                    vals = {
+                        "quantity": r.offer_amount,
+                        "order_id": self.id,
+                        "product_id":r.get_product.id
+                    }
+                    self.env["order.line"].create(vals)
+
+
 
 class product(models.Model):
     _name = 'order.line'
@@ -30,37 +43,15 @@ class product(models.Model):
 
     quantity = fields.Float(string="quantity", default=1.00)
     price = fields.Float(string="price")
-    discount = fields.Float(string="discount", readonly=True, store=True)
     total = fields.Float(string="total", compute="_total_price")
     order_id = fields.Many2one("coffee.order", string="order_id")
+    discount = fields.float(string="discount")
 
     @api.depends('price', 'quantity')
     def _total_price(self):
         for r in self:
-            if r.quantity > 2:
-                r.total = r.price * r.quantity - r.price * .1
-                r.discount = r.price * .1
-            else:
-                r.total = r.price * r.quantity
-                r.discount = 0.00
+            r.total = r.price * r.quantity
 
     @api.onchange('product_id')
     def _price_change(self):
         self.price = self.product_id.list_price
-
-    # @api.onchange('amount')
-    # def _discount_change(self):
-    #
-
-# class promotion(models.Model):
-#     _name = 'sale.promotion'
-#     _description = 'sale_promotion'
-#
-#     product = fields.Many2one("product.product", string="name of product",
-#                               ondelete='set null')
-#
-#     quantity = fields.Float(string="quantity", default=1.00)
-#     price = fields.Float(string="price")
-#     total = fields.Float(string="total", compute="_total_price")
-#     discount = fields.Float(string="discount", compute="_disc_", store=True)
-#     order_id = fields.Many2one("coffee.order", string="order_id")
