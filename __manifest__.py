3c2df57ab06445c1ee25df5cb88459712c97b5ec
eslammohamedhelmy-git <70444790+eslammohamedhelmy-git@users.noreply.github.com>
2020-09-07 16:10:13 +0200

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','sale'],
 
     # always loaded
     'data': [
@@ -29,6 +29,7 @@
         'reports/report.xml',
         'views/templates.xml',
         'views/sale_promotion.xml',
+        'views/sales.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
